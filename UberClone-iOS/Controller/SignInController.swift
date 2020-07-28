c9468962d6b2882a7b54ba819b7980284cc98adb
alanpcao@gmail.com <alanpcao@gmail.com>
2020-07-28 14:58:02 -0700

    XCode is being buggy

---
+++ UberClone-iOS/Controller/SignInController.swift
@@ -0,0 +1,18 @@
+//
+//  SignInController.swift
+//  UberClone-iOS
+//
+//  Created by Alan Cao on 7/28/20.
+//  Copyright © 2020 Alan Cao. All rights reserved.
+//
+
+import UIKit
+
+class SignInController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+    }
+}
