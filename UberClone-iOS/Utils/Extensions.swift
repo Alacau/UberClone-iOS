adc4ff1dddeb80a5b57997f1294ce26553d187c6
alanpcao@gmail.com <alanpcao@gmail.com>
2020-08-23 18:16:26 -0700

    Create extensions file for programmatic constraints

---
+++ UberClone-iOS/Utils/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  UberClone-iOS
+//
+//  Created by Alan Cao on 8/23/20.
+//  Copyright © 2020 Alan Cao. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func anchor() {
+        
+    }
+}
